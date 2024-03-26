dca1869c4460ab28148e863593f9da5142eb06b4
JOSOYA <whthdi4693@naver.com>
2024-03-26 13:44:11 +0900

    first commit

---
+++ src/test/java/com/soyajo/aboutspring/AboutSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soyajo.aboutspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
