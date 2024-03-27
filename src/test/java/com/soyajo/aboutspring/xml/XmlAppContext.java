5d62eb21e1a17fbf4dc876f81bc5335c607735f6
JOSOYA <whthdi4693@naver.com>
2024-03-27 11:20:55 +0900

    스프링 컨테이너를 이용한 싱글톤 패턴 구현

---
+++ src/test/java/com/soyajo/aboutspring/xml/XmlAppContext.java
@@ -0,0 +1,22 @@
+package com.soyajo.aboutspring.xml;
+
+import com.soyajo.aboutspring.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext {
+
+    @Test
+    @DisplayName("xml로 appConfig 개발")
+    void xmlAppContext() {
+        ApplicationContext ac = new  GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
