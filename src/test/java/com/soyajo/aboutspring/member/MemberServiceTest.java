8b39e957047c1137c2b1810a676383463d6ad3c2
JOSOYA <whthdi4693@naver.com>
2024-03-27 17:26:20 +0900

    이클립스로 툴 변경

--- src/test/java/com/soyajo/aboutspring/member/MemberServiceTest.java
+++ src/test/java/com/soyajo/aboutspring/member/MemberServiceTest.java
@@ -1,6 +1,5 @@
 package com.soyajo.aboutspring.member;
 
-
 import com.soyajo.aboutspring.AppConfig;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,24 +7,24 @@ import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService;
+	MemberService memberService;
 
-    @BeforeEach
-    public void beforeEach() {
-        AppConfig appConfig = new AppConfig();
-        memberService = appConfig.memberService();
-    }
+	@BeforeEach
+	public void beforeEach() {
+		AppConfig appConfig = new AppConfig();
+		memberService = appConfig.memberService();
+	}
 
-    @Test
-    void join() {
-        //given
-        Member member = new Member(1L, "memberA", Grade.VIP);
+	@Test
+	void join() {
+		// given
+		Member member = new Member(1L, "memberA", Grade.VIP);
 
-        //when
-        memberService.join(member);
-        Member findMember = memberService.findMember(1L);
+		// when
+		memberService.join(member);
+		Member findMember = memberService.findMember(1L);
 
-        //then
-        Assertions.assertThat(member).isEqualTo(findMember);
-    }
+		// then
+		Assertions.assertThat(member).isEqualTo(findMember);
+	}
 }
